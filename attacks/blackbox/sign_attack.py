812000baa287c742ac6b594131a5ed774f3caf17
sign4bb <46974839+sign4bb@users.noreply.github.com>
2019-03-14 17:23:52 -0400

    bug-fix

--- attacks/blackbox/sign_attack.py
+++ attacks/blackbox/sign_attack.py
@@ -40,43 +40,41 @@ class SignAttack(BlackBoxAttack):
     def _suggest(self, xs_t, loss_fct, metric_fct):
         _shape = list(xs_t.shape)
         dim = np.prod(_shape[1:])
-        num_axes = len(_shape[1:])
         # additional queries at the start
         add_queries = 0
         if self.is_new_batch:
             self.xo_t = xs_t.clone()
-            self.sgn_t = sign(ch.randn(_shape[0], dim))
             self.h = 0
             self.i = 0
-            add_queries = 1
+        if self.i == 0 and self.h == 0:
+            self.sgn_t = sign(ch.ones(_shape[0], dim))
+            fxs_t = lp_step(self.xo_t, self.sgn_t.view(_shape), self.epsilon, self.p)
+            bxs_t = self.xo_t  # - self.epsilon * self.sgn_t.view(_shape)
+            est_deriv = (loss_fct(fxs_t.cpu().numpy()) - loss_fct(bxs_t.cpu().numpy())) / self.epsilon
+            self.best_est_deriv = est_deriv
+            add_queries = 3 # because of bxs_t and the 2 evaluations in the i=0, h=0, case.
         chunk_len = np.ceil(dim / (2 ** self.h)).astype(int)
         istart = self.i * chunk_len
         iend = min(dim, (self.i + 1) * chunk_len)
         self.sgn_t[:, istart:iend] *= - 1.
-        fxs_t = self.xo_t + self.fd_eta * self.sgn_t.view(_shape)
-        bxs_t = self.xo_t #- self.epsilon * self.sgn_t.view(_shape)
-        est_deriv = (loss_fct(fxs_t.cpu().numpy()) - loss_fct(bxs_t.cpu().numpy())) / self.fd_eta
-        #print(est_deriv)
-        if self.i == 0 and self.h == 0:
-            self.best_est_deriv = est_deriv
-        else:
-            self.sgn_t[[i for i, val in enumerate(est_deriv < self.best_est_deriv) if val], istart: iend] *= -1.
-            self.best_est_deriv = (est_deriv >= self.best_est_deriv) * est_deriv + (est_deriv < self.best_est_deriv) * self.best_est_deriv
+        fxs_t = lp_step(self.xo_t, self.sgn_t.view(_shape), self.epsilon, self.p)
+        bxs_t = self.xo_t
+        est_deriv = (loss_fct(fxs_t.cpu().numpy()) - loss_fct(bxs_t.cpu().numpy())) / self.epsilon
+        self.sgn_t[[i for i, val in enumerate(est_deriv < self.best_est_deriv) if val], istart: iend] *= -1.
+        self.best_est_deriv = (est_deriv >= self.best_est_deriv) * est_deriv + (est_deriv < self.best_est_deriv) * self.best_est_deriv
         # compute the cosine similarity
         cos_sims, ham_sims = metric_fct(self.xo_t.cpu().numpy(), self.sgn_t.cpu().numpy())
-        #print(cos_sims, ham_sims)
         # perform the step
-        new_xs = linf_step(xs_t, self.sgn_t.view(_shape), self.epsilon)
+        new_xs = lp_step(self.xo_t, self.sgn_t.view(_shape), self.epsilon, self.p)
         # update i and h for next iteration
         self.i += 1
-        if self.i == 2 ** self.h or istart >= dim:
+        if self.i == 2 ** self.h or iend == dim:
             self.h += 1
             self.i = 0
             # if h is exhausted, set xo_t to be xs_t
             if self.h == np.ceil(np.log2(dim)).astype(int) + 1:
-                self.xo_t = xs_t
+                self.xo_t = xs_t.clone()
                 self.h = 0
-                add_queries = 1 # because it will be queried next round at bxs_t
                 print("new change")
         return new_xs, np.ones(_shape[0]) + add_queries, cos_sims, ham_sims
 
